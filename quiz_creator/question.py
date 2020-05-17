8cfb8630d6c689cdb8fca58c8b6d9581a4494c90
rijun <richard.jung001@stud.fh-dortmund.de>
2020-05-17 10:29:32 +0200

    Replace Python project with C++ project

--- quiz_creator/question.py
+++
@@ -1,5 +0,0 @@
-class Question:
-    def __init__(self):
-        self.question = ""
-        self.answer = ""
-        self.options = []
--- quiz_creator/quiz_category.py
+++
@@ -1,9 +0,0 @@
-class QuizCategory:
-    def __init__(self, name):
-        self.name = name
-        self.questions = {
-            'easy': [],
-            'medium': [],
-            'hard': [],
-            'extreme': []
-        }
