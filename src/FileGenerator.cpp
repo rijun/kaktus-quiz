e05b4f3a002eee5ef19694e5616554681ed54c70
rijun <richard.jung001@stud.fh-dortmund.de>
2020-06-12 16:14:17 +0200

    Expand template

--- src/FileGenerator.cpp
+++ src/FileGenerator.cpp
@@ -2,6 +2,8 @@
 #include "lib/inja.hpp"
 #include "lib/json.hpp"
 
+#include <QDebug>
+
 FileGenerator::FileGenerator()
 {
 
@@ -9,21 +11,14 @@ FileGenerator::FileGenerator()
 
 bool FileGenerator::generateQuiz(const std::string &quizData, const std::string &templateFile, const std::string &targetFolder)
 {
-    nlohmann::json j = nlohmann::json::parse(quizData);
     inja::Environment env;
 
-//    try {
-      inja::Template temp = env.parse(templateFile);
-      env.write(temp, j[0], targetFolder + "/test.html");
-
-
-//        std::string result = env.render("{{ question }}", j);
-//        std::cout << result;
-//    } catch (std::runtime_error) {
-//        return false;
-//    }
-
-    // Render a string with json data
+    try {
+        inja::Template temp = env.parse(templateFile);
+        env.write(temp, nlohmann::json::parse(quizData)[0], targetFolder + "/test.html");
+    } catch (std::runtime_error) {
+        return false;
+    }
 
     return true;
 }
