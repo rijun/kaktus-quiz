5cdb3e607d6c360cc7205d455ea3639bb48bf643
chrjun <junchr@t-online.de>
2021-04-14 12:56:35 +0200

    Finished working quiz app

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -30,7 +30,7 @@ export default {
     return {
       questions: [],
       loading: true,
-      index: 0
+      index: 0,
     };
   },
   computed: {
@@ -39,33 +39,167 @@ export default {
         return this.questions[this.index];
       }
       return null;
-    }
+      //Keyword this usually refers to the Vue Component Instance, e.g. this.questions points
+      //to the questions array in the data() function
+    },
+    score() {
+      if (this.questions !== []) {
+        // Here, we want to collect data in an object about the users statistics - later be emitted on an event when users finishes quiz
+        return {
+          allQuestions: this.questions.length,
+          answeredQuestions: this.questions.reduce((count, currentQuestion) => {
+            if (currentQuestion.userAnswer) {
+              // userAnswer is set when user has answered a question, no matter if right or wrong
+              count++;
+            }
+            return count;
+          }, 0),
+          correctlyAnsweredQuestions: this.questions.reduce(
+            (count, currentQuestion) => {
+              if (currentQuestion.rightAnswer) {
+                // rightAnswer is true, if user answered correctly
+                count++;
+              }
+              return count;
+            },
+            0
+          ),
+        };
+      } else {
+        return {
+          allQuestions: 0,
+          answeredQuestions: 0,
+          correctlyAnsweredQuestions: 0,
+        };
+      }
+    },
+    correctAnswers() {
+      if (this.questions && this.questions.length > 0) {
+        let streakCounter = 0;
+        this.questions.forEach(function(question) {
+          if (!question.rightAnswer) {
+            return;
+          } else if (question.rightAnswer === true) {
+            streakCounter++;
+          }
+        });
+        return streakCounter;
+      } else {
+        return "--";
+      }
+    },
+    pluralizeAnswer() {
+      // For grammatical correctness
+      return this.correctAnswers === 1 ? "Answer" : "Answers";
+    },
+    quizCompleted() {
+      if (this.questions.length === 0) {
+        return false;
+      }
+      /* Check if all questions have been answered */
+      let questionsAnswered = 0;
+      this.questions.forEach(function(question) {
+        question.rightAnswer !== null ? questionsAnswered++ : null;
+      });
+      return questionsAnswered === this.questions.length;
+    },
   },
-  //Keyword this usually refers to the Vue Component Instance, e.g. this.questions points
-  //to the questions array in the data() function
+  watch: {
+    quizCompleted(completed) {
+      /*
+       * Watcher on quizCompleted fires event "quiz-completed"
+       * up to parent App.vue component when completed parameter
+       * returned by quizCompleted computed property true
+       */
+      completed &&
+        setTimeout(() => {
+          this.$emit("quiz-completed", this.score);
+        }, 3000); // wait 3 seconds until button animation is over
+    },
+  },
   methods: {
     async fetchQuestions() {
       this.loading = true;
-      //fetch questions
       let response = await fetch(
-        "https://opentdb.com/api.php?amount=10&category=9"
+        "https://opentdb.com/api.php?amount=5&category=9"
       );
       //convert questions into json
       let jsonResponse = await response.json();
+      let index = 0; // index is used to identify single answer
       //manipulate questions
       let data = jsonResponse.results.map((question) => {
+        // put answers on question into single array
         question.answers = [
           question.correct_answer,
-          ...question.incorrect_answers
+          ...question.incorrect_answers,
           /*The tree dots go to the existing object and get all its properties, copies these and then overwrite
             explicitly the other properties defined (https://oprea.rocks/blog/what-do-the-three-dots-mean-in-javascript) */
         ];
+        /* Shuffle question.answers array */
+        for (let i = question.answers.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [question.answers[i], question.answers[j]] = [
+            question.answers[j],
+            question.answers[i],
+          ];
+        }
+        // mention in Step 1
+        question.rightAnswer = null;
+        question.key = index;
+        index++;
         return question;
       });
-      //put data on question property
       this.questions = data;
       this.loading = false;
-      console.log(data);
+    },
+    handleButtonClick: function(event) {
+      /* Find index to identiy question object in data */
+      let index = event.target.getAttribute("index");
+      let pollutedUserAnswer = event.target.innerHTML; // innerHTML is polluted with decoded HTML entities e.g ' from &#039;
+      /* Clear from pollution with ' */
+      let userAnswer = pollutedUserAnswer.replace(/'/, "&#039;");
+      /* Set userAnswer on question object in data */
+      this.questions[index].userAnswer = userAnswer;
+      /* Set class "clicked" on button with userAnswer -> for CSS Styles; Disable other sibling buttons */
+      event.target.classList.add("clicked");
+      let allButtons = document.querySelectorAll(`[index="${index}"]`);
+      for (let i = 0; i < allButtons.length; i++) {
+        if (allButtons[i] === event.target) continue;
+        allButtons[i].setAttribute("disabled", "");
+      }
+      /* Invoke checkAnswer to check Answer */
+      this.checkAnswer(event, index);
+    },
+    checkAnswer: function(event, index) {
+      let question = this.questions[index];
+      if (question.userAnswer) {
+        if (this.index < this.questions.length - 1) {
+          setTimeout(
+            function() {
+              this.index += 1;
+            }.bind(this),
+            3000
+          );
+        }
+        if (question.userAnswer === question.correct_answer) {
+          /* Set class on Button if user answered right, to celebrate right answer with animation joyfulButton */
+          event.target.classList.add("rightAnswer");
+          /* Set rightAnswer on question to true, computed property can track a streak out of 10 questions */
+          this.questions[index].rightAnswer = true;
+        } else {
+          /* Mark users answer as wrong answer */
+          event.target.classList.add("wrongAnswer");
+          this.questions[index].rightAnswer = false;
+          /* Show right Answer */
+          let correctAnswer = this.questions[index].correct_answer;
+          let allButtons = document.querySelectorAll(`[index="${index}"]`);
+          allButtons.forEach(function(button) {
+            if (button.innerHTML === correctAnswer) {
+              button.classList.add("showRightAnswer");
+            }
+          });
+        }
+      }
     },
   },
   //Code inside mounted() runs after the Component has mounted
@@ -83,6 +217,7 @@ body {
   margin: 1rem auto;
   padding: 1rem;
   max-width: 750px;
+  background-color: slategray;
 }
 
 #logo-headline {
