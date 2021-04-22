a68463a1c122586fa16ada66b332bedd50378da2
chrjun <junchr@t-online.de>
2021-04-22 12:21:56 +0200

    Selects random id

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,9 @@
     to="/selection"
     class="btn btn-lg btn-primary btn-start"
     tag="button"
-    @click="startQuiz"
     >Quiz starten</router-link
   >
-  <Button text="TEst" @click="startQuiz"/>
+  <button v-on:click="selectTeam">Get Weather Data</button>
 </template>
 
 <script>
@@ -26,12 +25,43 @@ export default {
   data() {
     return {
       teams: [],
+      weatherDataList: [],
     };
   },
   methods: {
-    startQuiz() {
-      
-      console.log("Blin");
+    selectTeam() {
+      //here I am fetching all the data from the teams json file
+      fetch("http://localhost:5001/teams")
+        .then((response) => response.json())
+        .then((data) => (this.weatherDataList = data));
+      //here i am going through the json and save all IDs in an array
+      let idList = [];
+      this.weatherDataList.forEach((teamID) => {
+        console.log(teamID.id);
+        idList.push(teamID.id);
+      });
+      //here i am generating a random number and select array position
+      var obj_keys = Object.keys(idList);
+      var ran_key = obj_keys[Math.floor(Math.random() * obj_keys.length)];
+      let selectedID = idList[ran_key];
+      console.log("SELECTED " + selectedID);
+
+      /*  const toggleTask = await this.fetchTask(id);
+      const updateTask = { ...toggleTask, reminder: !toggleTask.reminder };
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updateTask),
+      });
+
+      const data = await res.json();
+
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      ); */
     },
     async addTeam(team) {
       const res = await fetch("http://localhost:5001/teams", {
