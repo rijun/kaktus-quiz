25d41ec1ed712eb41f6d116d8a3607409c73bbc1
chrjun <JunChr@t-online.de>
2021-05-13 11:29:01 +0200

    Meh

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -139,9 +139,17 @@ export default {
     async fetchQuestions() {
       this.loading = true;
       let response = await fetch(
-        "https://opentdb.com/api.php?amount=3&category=31&difficulty=" +
+        "https://opentdb.com/api.php?amount=3&category=12&difficulty=" +
           this.difficulty
       );
+      const ress = await fetch("http://localhost:5002/results")
+      const da = await ress.json();
+      console.log(da)
+
+
+
+
+
       //convert questions into json
       let jsonResponse = await response.json();
       let index = 0; // index is used to identify single answer
